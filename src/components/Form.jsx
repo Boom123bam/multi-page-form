ef0cb71418ac0e8e741576925375fb22b0c69a3c
Boom123Bam <brandontang0325@gmail.com>
2023-08-25 23:10:52 +0100

    building first page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Form.css";
+import Input from "./Input";
 
 export default function Form() {
   const [step, setStep] = useState(1);
@@ -8,40 +9,40 @@ export default function Form() {
       <div className="left-section">
         <div className="left-section-content">
           <div className="step">
-            <div className="step-circle">
+            <div className={`step-circle${step == 1 ? " fill" : ""}`}>
               <span>1</span>
             </div>
-            <div className="step-text desktop">
+            <div className="text desktop">
               <h4>Step 1</h4>
               <h2 className="bold">Your Info</h2>
             </div>
           </div>
 
           <div className="step">
-            <div className="step-circle fill">
+            <div className={`step-circle${step == 2 ? " fill" : ""}`}>
               <span>2</span>
             </div>
-            <div className="step-text desktop">
+            <div className="text desktop">
               <h4>Step 2</h4>
               <h2 className="bold">Select Plan</h2>
             </div>
           </div>
 
           <div className="step">
-            <div className="step-circle">
+            <div className={`step-circle${step == 3 ? " fill" : ""}`}>
               <span>3</span>
             </div>
-            <div className="step-text desktop">
+            <div className="text desktop">
               <h4>Step 3</h4>
               <h2 className="bold">Add Ons</h2>
             </div>
           </div>
 
           <div className="step">
-            <div className="step-circle">
+            <div className={`step-circle${step == 4 ? " fill" : ""}`}>
               <span>4</span>
             </div>
-            <div className="step-text desktop">
+            <div className="text desktop">
               <h4>Step 4</h4>
               <h2 className="bold">Summary</h2>
             </div>
@@ -50,10 +51,8 @@ export default function Form() {
       </div>
 
       <div className="right-section">
-        <div className="form-content"></div>
-        <div className="buttons">
-          <button className="back">Go Back</button>
-          <button className="next">Next Step</button>
+        <div className="form-content">
+          <Input step={step} setStep={setStep} />
         </div>
       </div>
     </div>
